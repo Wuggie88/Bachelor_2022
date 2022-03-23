4453bd00829c41e01c0fbed5385cc9e9a44109a6
Wuggie <wuggie88@yahoo.co.uk>
2022-03-23 13:32:38 +0100

    quik maffs

---
+++ Unity_Bachelor/Assets/Assignment.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Assignment : MonoBehaviour
+{
+
+    public string answer;
+    public Text AssText;
+    public float x;
+    public float y;
+
+    public enum myEnums {ShootPyt1, ShootPyt2, ShootDiv1, ShootDiv2, HorseMul1, HorseMul2, HorseDiv1, HorseDiv2};
+
+    public myEnums MyEnums;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartUp());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator StartUp()
+    {
+        switch (MyEnums)
+        {
+            case myEnums.ShootPyt1:
+                x = 80;
+                y = 45;
+                AssText.text = "Pyt1";
+                answer = (System.Math.Round(Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2)), 2)).ToString();
+                break;
+
+            case myEnums.ShootPyt2:
+                x = Random.Range(10, 100);
+                y = Random.Range(10, 100);
+                AssText.text = "Your two numbers are " + x + " and " + y;
+                answer = (System.Math.Round(Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(y, 2)), 2)).ToString();
+                break;
+
+                    
+        }
+
+
+        yield return new WaitForEndOfFrame();
+    }
+    
+}
---
+++ Unity_Bachelor/Assets/CheckAnswer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckAnswer : MonoBehaviour
+{
+
+    public string answer;
+    public string fieldAns;
+    public GameObject AnsObject;
+    public GameObject AnsField;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnsObject = GameObject.Find("AssignmentTxt");
+        AnsField = GameObject.Find("AnsField");
+        answer = "DGADGAIEGQPIEGH";
+        StartCoroutine(GetAns());
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator GetAns()
+    {
+        yield return new WaitForSeconds(5);
+        answer = AnsObject.GetComponent<Assignment>().answer;
+    }
+
+    public void CheckAns() {
+        StartCoroutine(Check());
+    }
+
+    IEnumerator Check()
+    {
+        InputField input = AnsField.GetComponent<InputField>();
+        fieldAns = input.text;
+        yield return new WaitForEndOfFrame();
+
+        if(fieldAns == answer)
+        {
+            //correct answer!
+            Debug.Log("Correct");
+        }
+        else
+        {
+            //incorrect
+            Debug.Log("Not correct, actually wrong");
+        }
+        
+
+
+    }
+}
